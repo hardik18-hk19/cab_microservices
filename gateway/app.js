e8caad5aadb03eceaa20c51dbfe4ee1c29a8be9c
Hardik Agarwal <hardikagarwal9308@gmail.com>
2025-07-23 00:25:32 +0530

    feat: add morgan for logging middleware in gateway

--- gateway/app.js
+++ gateway/app.js
@@ -1,7 +1,10 @@
 import express from "express";
 import expressProxy from "express-http-proxy";
+import morgan from "morgan";
 
 const app = express();
+
+app.use(morgan("dev"));
 
 app.use("/user", expressProxy("http://localhost:3001"));
 app.use("/captain", expressProxy("http://localhost:3002"));
