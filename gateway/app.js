5962e2468669ceba4147625d2e6ba6e3695aaa26
Hardik Agarwal <hardikagarwal9308@gmail.com>
2025-07-22 23:36:59 +0530

    feat: add proxy route for captain service in gateway

--- gateway/app.js
+++ gateway/app.js
@@ -4,6 +4,7 @@ import expressProxy from "express-http-proxy";
 const app = express();
 
 app.use("/user", expressProxy("http://localhost:3001"));
+app.use("/captain", expressProxy("http://localhost:3002"));
 
 app.listen(3000, () => {
   console.log("Gateway running on port 3000");
