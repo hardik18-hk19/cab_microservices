13628fa9b42092847f2e4de5a2399cd545b77628
Hardik Agarwal <hardikagarwal9308@gmail.com>
2025-07-22 19:36:26 +0530

    feat: add database connection utility and integrate it into the app

--- user/app.js
+++ user/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import userRoutes from "./routes/user.routes.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import connectDb from "./config/db.js";
 
 dotenv.config();
 
@@ -10,6 +11,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+connectDb();
 
 app.use("/", userRoutes);
 
