d43ddd5b34efc5a721612b78ecb7e3c8a70df5d0
Hardik Agarwal <hardikagarwal9308@gmail.com>
2025-07-24 00:06:18 +0530

    feat: add RabbitMQ connection and messaging service

--- ride/app.js
+++ ride/app.js
@@ -3,10 +3,13 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import RideRoutes from "./routes/ride.routes.js";
 import connectDb from "./config/db.js";
+import { connectRabbit } from "./service/rabbit.js";
 
 dotenv.config();
 
 const app = express();
+
+connectRabbit();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
